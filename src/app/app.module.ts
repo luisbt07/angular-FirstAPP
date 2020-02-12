320320e09230d795e405255d90e190fcc24eedc9
luisbt07 <luisbt_07@hotmail.com>
2020-02-12 10:36:28 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -11,7 +12,7 @@ import { ProductAlertsComponent} from './product-alerts/product-alerts.component
 import { ProductDetailsComponent} from './product-details/product-details.component'; 
 import { CartComponent} from './cart/cart.component';
 import {ShippingComponent} from './shipping/shipping.component';
-
+import {CartService} from './cart.service';
 
 @NgModule({
   imports: [
@@ -34,9 +35,12 @@ import {ShippingComponent} from './shipping/shipping.component';
     CartComponent,
     ShippingComponent
   ],
+  providers: [
+    CartService
+  ],
   bootstrap: [
     AppComponent
-  ]
+  ] 
 })
 export class AppModule { }
 
